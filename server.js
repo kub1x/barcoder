445b9ecd289b8d400b439fbeafd38e96818bce78
Jakub Podlaha <j.podlaha@gmail.com>
2020-02-17 13:09:38 +0100

    added fallback generating using CODE128

--- server.js
+++ server.js
@@ -5,15 +5,20 @@ const PORT = process.env.PORT || 3000;
 const JsBarcode = require('jsbarcode');
 const { createCanvas } = require('canvas');
 
-app.get('/:ean.png', (req, res) => {
+async function generateBarcode({ ean, format }) {
   const canvas = createCanvas(500, 1000);
+  await JsBarcode(canvas, ean, { format });
+  return canvas;
+};
+
+app.get('/:ean.png', async (req, res) => {
   const { ean } = req.params;
   console.log('-- generating barcode for ean: ' + ean);
-  try {
-    JsBarcode(canvas, ean, { format:"EAN13" });
-  } catch (e) {
-    JsBarcode(canvas, ean, { format:"EAN8" });
-  }
+
+  const canvas = await (generateBarcode({ ean, format: "EAN13" })
+           .catch(() => generateBarcode({ ean, format: "EAN8" }))
+           .catch(() => generateBarcode({ ean, format: "CODE128" })));
+
   const mimeType = 'image/png';
   const resultBuffer = canvas.toBuffer(mimeType);
   res.writeHead(200, {'Content-Type': mimeType});
